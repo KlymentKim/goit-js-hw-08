e5811d687bc0e1b692f6c4fe852930670cc85ff3
Klyment <49413026+klim86klim@users.noreply.github.com>
2023-04-04 14:57:18 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
+import throttle from 'lodash.throttle';
 
+const form = document.getElementsByClassName('feedback-form');
+const emailInput = document.getElementsByTagName('input[name="email]');
+console.log(emailInput);
+const messageInput  = document.getElementsByTagName('textarea[name="message"]');
+console.log(messageInput);
